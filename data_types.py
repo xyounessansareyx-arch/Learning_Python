bf9ddc1b34cf3e238d4e2a795c6defd8e274a012
xyounessansareyx-arch <xyounessansareyx@gmail.com>
2026-09-24 11:54:39 +0330

    This an introduction to all data types in Python.

---
+++ data_types.py
@@ -0,0 +1,30 @@
+# This is a document of all data types in Python I know
+
+# Integer Numbers
+number = 14
+
+# Floating Point
+float_number = 3.1415
+
+# Complex Numbers
+x = 3
+y = 4
+z = complex(x, y)
+
+# Strings
+name = 'Youness'
+
+# Lists
+my_list = ['Youness', 25, 3.1415, 'Ansarey']
+
+# Sets
+my_tuple = ['Youness', 25, 3.1415, 'Ansarey', 'Youness']
+
+# Booleans
+is_alive = True
+
+# Dictionaries
+my_tuple = {'First_Name':'Youness', 'Age': 25, 'Favorite_Number': 3.1415, 'Last_Name': 'Ansarey'}
+
+# Tuples (Immutable)
+my_tuple = ('Youness', 25, 3.1415, 'Ansarey')
