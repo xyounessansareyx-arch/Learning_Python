21161a8d7593382f12217a34b4af4a2a3a41c452
xyounessansareyx-arch <xyounessansareyx@gmail.com>
2026-09-25 21:31:07 +0330

    Learning how to work with dictionaries. Some codes don't work yet but I'm working on it.

---
+++ 1. Introduction to Python/working_with_dictionary.py
@@ -0,0 +1,16 @@
+my_dict = {
+    "first_name": "Ryan",
+    "last_name": "Reynolds",
+    "courses": {
+        "mathematics":5, 
+        "programmnig": 0,
+        "probability": 5,
+        "business": 20,
+        "art": 20
+        }
+}
+
+# print(my_dict["courses"])
+my_dict.items()
+my_dict.get('courses')
+my_dict['first_name']
