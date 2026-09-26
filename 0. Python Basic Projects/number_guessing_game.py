3830efe1d5bfa51f102a18f3697b2bd26e2b3271
xyounessansareyx-arch <xyounessansareyx@gmail.com>
2026-09-26 21:49:44 +0330

    Start thinking about the number guessing game in Python. I'll complete it tomorrow.

--- 0. Python Basic Projects/number_guessing_game.py
+++ 0. Python Basic Projects/number_guessing_game.py
@@ -1,1 +1,26 @@
-# This is Just a comment
+'''
+Number Guessing Game is a game in which the computer randomly selects a number between 1 and 100. 
+The user must guess the number within a limited number of attempts. After each incorrect guess, 
+the program provides a hint indicating whether the guess is too low or too high.
+(GeeksforGeeks)
+'''
+
+import random
+
+number = random.randrange(1, 100, 1)
+print(number)
+
+attemps = 7
+
+while True:
+    for i in range(1, attemps):
+        print(f'Attemp [{i}] of [{attemps}]')
+        user_number = input('Please enter a number: ')
+        if user_number.isdigit():
+            print('It is a number!')
+            break
+        else:
+            i =- 1
+            print('it is not a number!')
+            continue
+
